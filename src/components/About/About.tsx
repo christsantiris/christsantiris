0eb6df9951052cfe56e7ce5dbaee5b748319f218
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-09-11 11:17:19 -0400

    Update about me section text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,14 +5,17 @@ const About: React.FC = () => (
   <div className='about section-header' id='about'>
     <h1>About Me</h1>
     <div className='me'>
-      <p>Hi, I'm Chris, a full stack software engineer from the St Pete/Tampa Bay Area.</p>
-      <p> My favorite coding language is probably Typescript, but I enjoy writing code in Python and C# too.</p>
-      <p>Currently, I work at PwC where I am a manager and senior developer on several projects.</p>
-      <p>I go back and forth between working on the ci cd pipelines and delivering application features.</p>
-      <p>Outside of work, I like to be out on the water with my kayak or paddle board, or out riding my bike.</p>
+      <label>"By Day":</label>
+      <p>I am an IT manager and developer. I work with several teams creating web applications in Nodejs, Dotnet Core, or Python.</p>
+      <p> I can write code in JavaScript, Typescript, Python, and C# using libraries like Node.js, Flask, Django, Angular or React.</p>
+      <p>Currently, I'm passively keeping an eye out for a role in an exciting area of technology like Machine Learning, Blockchain, or Cybersecurity.</p>
+      <label>"By Night":</label>
+      <p> I love to kayak, paddle board, or fish and be out on the water. I code for fun and I also love board games and card games.</p>
       <p>Check out my social media links at the bottom of the page or email me at <a href="mailto:chris.tsantiris@gmail.com?Subject=Hi%20Chris" target="_top">chris.tsantiris@gmail.com</a></p>
     </div>
   </div>
 );
 
 export default About;
+
+
